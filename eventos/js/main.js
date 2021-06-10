63a456ccde5b60bb065db7d5802f4b071cb413c3
Luis Fernando Espinoza Mora <71098673+LoF002@users.noreply.github.com>
2021-06-10 15:18:27 -0600

    Frontend
    Heroku

---
+++ eventos/js/main.js
@@ -0,0 +1,20 @@
+let nav = document.body.getElementsByClassName('top-nav')[0];
+
+let modal = document.body.getElementsByClassName('modal')[0];
+
+function openMobileMenu(){
+    nav.style.left="0";
+    modal.style.display="block";
+}//Fin funtion
+
+function closeMobileMenu(){
+    nav.style.left="-24em";
+    modal.style.display="none";
+}//Fin funtion
+
+window.onclick = function(e){
+    if(e.target == modal){
+        modal.style.display="none";
+        nav.style.left="-24em";
+    }
+}
